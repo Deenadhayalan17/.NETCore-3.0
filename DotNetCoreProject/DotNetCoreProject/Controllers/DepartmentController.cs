a1fa995699fb84e3a03c757c99c6e45b786cba10
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-05-09 23:29:31 +0530

    initil

---
+++ DotNetCoreProject/DotNetCoreProject/Controllers/DepartmentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreProject.Controllers
+{
+    [ApiController]
+    [Route(template:"[controller]")]
+    public class DepartmentController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("get all the stident");
+        }
+        
+        [HttpGet(template:"{code}")]
+        public IActionResult GetA( string code)
+        {
+            return Ok("get" + code + " data");
+        }
+        [HttpPost]
+        public IActionResult Insert()
+        {
+            return Ok("insert sucess");
+        }
+        [HttpPut(template: "{code}")]
+        public IActionResult update(string code)
+        {
+            return Ok("update" + code + " data");
+        }
+        [HttpDelete(template: "{code}")]
+        public IActionResult delete( string code)
+        {
+            return Ok("delete" + code + " data");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# NET COre 3.0 Project
