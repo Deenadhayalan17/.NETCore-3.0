7638f6cc3324346ca8b788f33ed138e66ea4b806
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-05-10 11:08:45 +0530

    Implementing Swaggger

--- DotNetCoreProject/DotNetCoreProject/Program.cs
+++ DotNetCoreProject/DotNetCoreProject/Program.cs
@@ -14,6 +14,7 @@ namespace DotNetCoreProject
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -21,6 +22,7 @@ namespace DotNetCoreProject
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
                 });
     }
 }
--- DotNetCoreProject/DotNetCoreProject/Startup.cs
+++ DotNetCoreProject/DotNetCoreProject/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,33 @@ namespace DotNetCoreProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            setupSwagger(services);
+        }
+
+        private void setupSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Shayne Boyer",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,7 +63,11 @@ namespace DotNetCoreProject
             {
                 app.UseDeveloperExceptionPage();
             }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.)
+            app.UseSwaggerUI();
             app.UseHttpsRedirection();
 
             app.UseRouting();
