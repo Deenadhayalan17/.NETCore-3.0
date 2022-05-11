f1c71ea3af81a078a50d0774a6ee508d27838a5f
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-05-11 11:33:39 +0530

    Add project files.

---
+++ DotNetCoreProject/Controllers/DepartmentController.cs
@@ -0,0 +1,85 @@
+﻿using DotNetCoreProject.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace DotNetCoreProject.Controllers
+{
+    //[Route(template: "[controller]")]
+    public class DepartmentController : MainApiController
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(DepartmentStatic.GetAllDepartment());
+        }
+
+        [HttpGet(template: "{code}")]
+        public IActionResult GetA(string code)
+        {
+            return Ok(DepartmentStatic.GetADepartment());//string code
+        }
+        [HttpPost]
+        public IActionResult Insert( Department department)
+        {
+            return Ok(DepartmentStatic.InsertDepartment(department));
+        }
+        [HttpPut(template: "{code}")]
+        public IActionResult update(string code,Department department)
+        {
+            return Ok(DepartmentStatic.UpdateDepartment(code,department));
+        }
+        [HttpDelete(template: "{code}")] 
+        public IActionResult Delete(string code)
+        {
+            return Ok(DepartmentStatic.DeleteDepartment(code) );
+        }
+    }
+    public static class DepartmentStatic
+    {
+        public static List<Department> AllDepartment { get; set; } = new List<Department>();
+
+        public static Department InsertDepartment(Department department)
+        {
+            AllDepartment.Add(department);
+            return department;
+        }
+        public static List<Department> GetAllDepartment()
+        {
+            return AllDepartment;
+        }
+        public static Department GetADepartment()//string code
+        {
+            return AllDepartment.First();
+           // return AllDepartment.FirstOrDefault(x: Department => x.code == code);
+        }
+
+        public static Department UpdateDepartment(string code, Department department)
+        {
+            foreach(var i in AllDepartment)
+            {
+                if (code == i.code)
+                {
+                    i.name = department.name;
+                }
+            }
+            return department;
+        }
+
+        public static Department DeleteDepartment(string code)
+        {
+            //var department = AllDepartment.FirstOrDefault(x: department => x.code == code);
+            //AllDepartment = AllDepartment.Where(x: department => x.Code != department.code).ToList();
+            //return department;
+            return AllDepartment.First();
+        }
+
+
+
+
+
+    }
+}
---
+++ DotNetCoreProject/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using DotNetCoreProject.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreProject.Controllers
+{
+
+    public class StudentController : MainApiController
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+
+        {
+            return Ok(StudentStatic.Getstudents());
+        }
+
+        [HttpPost]
+        public IActionResult insertdata(Student student)
+
+        {
+            return Ok(StudentStatic.InsertStudent(student));
+        }
+    }
+    public static class StudentStatic
+    {
+        public static List<Student> AllStudents { get; set; } = new List<Student>();
+
+        public static List<Student> Getstudents()
+        {
+            return AllStudents;
+        }
+
+        public static Student InsertStudent(Student student)
+        {
+
+            AllStudents.Add(student);
+            return student;
+        }
+    } 
+}
